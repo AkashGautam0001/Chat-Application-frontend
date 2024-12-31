70859b3996131ad37fe8f146ffba9968f0924e8e
Akash Gautam <akashgautam01584@gmail.com>
2024-12-31 15:37:15 +0530

    refactor: change API_URL export to a constant

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = `https://scalable-chat-app-qcq3.onrender.com/api`;
+const API_URL = `https://scalable-chat-app-qcq3.onrender.com/api`;
 
 export const fetchUser = (userId) => axios.get(`${API_URL}/user/${userId}`);
 export const fetchSessions = () => axios.get(`${API_URL}/chatSession`);
